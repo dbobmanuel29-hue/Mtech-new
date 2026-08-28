fcebfbd91e61154cdd288fc707473bdffa467dd1
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-08-28 09:19:09 +0100

    Load Cloudinary media metadata bridge before admin modules

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,7 @@
 /* M-TECH admin loader: preserve the existing dashboard core while adding the secure Users module. */
 (function () {
   function load(src) { document.write('<script src="' + src + '"><\/script>'); }
+  load("/js/cloudinary-firestore.js");
   load("/admin-core.js");
   load("/admin-users.js");
 })();
