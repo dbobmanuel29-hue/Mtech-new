85e2178f6f1b0db19ea229b03ef492fbab7863a8
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-08-28 07:18:55 +0100

    Clean up duplicate admin loader

--- admin.js
+++
@@ -1,6 +0,0 @@
-/* M-TECH admin loader: preserve the existing dashboard core while adding the secure Users module. */
-(function () {
-  function load(src) { document.write('<script src="' + src + '"><\/script>'); }
-  load("/admin-core.js");
-  load("/admin-users.js");
-})();
