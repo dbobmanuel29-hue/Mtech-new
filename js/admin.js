6f31a3ba32135de265de0e9bd03fe1afcb84abb7
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-08-31 13:01:56 -0700

    Apply permanent category admin UI policy

--- js/admin.js
+++ js/admin.js
@@ -1,9 +1,10 @@
 /* M-TECH admin loader: preserve the existing dashboard core while adding the secure Users module. */
 (function () {
-  function load(src) { document.write('<script src="' + src + '"><\/script>'); }
+  function load(src) { document.write('<script src="' + src + '"><\\/script>'); }
   load("/js/cloudinary-firestore.js");
   load("/js/admin-firestore-online.js");
   load("/admin-core.js");
   load("/admin-users.js");
   load("/js/admin-crud-repair.js");
+  load("/js/admin-ui-fix.js");
 })();
